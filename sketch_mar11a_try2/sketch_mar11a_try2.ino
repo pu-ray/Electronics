eb44fb65982a70768b8a826f9191b0e0cc44f77f
mbuguapurity <mbuguapurity1@gmail.com>
2019-03-27 07:38:36 +0300

    addfiles

--- sketch_mar11a_try2/sketch_mar11a_try2.ino
+++ sketch_mar11a_try2/sketch_mar11a_try2.ino
@@ -6,19 +6,26 @@ void setup() {
   pinMode (LEDGREEN,OUTPUT);
   pinMode (LEDRED,OUTPUT);
   Serial.print("This is my number 1 program");
-  // put your setup code here, to run once:
+  Serial.println(" ");
 
 }
 
 void loop() {
+  
+  Serial.print("blink # 1 is RED");
+  Serial.println(" ");
+  
   digitalWrite(LEDRED,HIGH);
   delay(1000);
 
   digitalWrite(LEDRED,LOW);
   delay(1000);
 
-  Serial.println("blink # 1 is RED");
-
+  
+  
+  
+ Serial.print("blink # 2 is GREEN");
+ Serial.println(" ");
   digitalWrite(LEDGREEN,HIGH);
   delay(1000);
   digitalWrite(LEDGREEN,LOW);
@@ -28,9 +35,9 @@ void loop() {
  delay(1000);
  digitalWrite(LEDGREEN,LOW);
  delay(1000);
-
- Serial.println("blink # 2 is GREEN");
-
+ 
+ Serial.print("blink # 3 is RED");
+ Serial.println(" ");
  digitalWrite(LEDRED,HIGH);
  delay(1000);
  digitalWrite(LEDRED,LOW);
@@ -45,11 +52,7 @@ void loop() {
  delay(1000);
  digitalWrite(LEDGREEN,LOW);
  delay(1000);
+ Serial.println(" ");
 
-
- Serial.println("blink # 3 is RED");
-
- 
-  // put your main code here, to run repeatedly:
 
 }
